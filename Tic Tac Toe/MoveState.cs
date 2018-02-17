33e59788080f1604f2c27e1c3d1e1ce5c7ae565d
Thanos Paravantis <p16112@students.cs.unipi.gr>
2018-02-17 17:45:18 +0200

    Finish comments

--- Tic Tac Toe/MoveState.cs
+++ Tic Tac Toe/MoveState.cs
@@ -2,6 +2,15 @@
 {
     public enum MoveState
     {
-        Win, Draw, Continue
+        // This enumerator defines all possible states that can be the result of a move made by a player.
+
+        // The win state means that the move resulted in a win.
+        Win,
+
+        // The draw state means that the move resulted to a draw.
+        Draw,
+
+        // The continue state means that the match will be continued.
+        Continue
     }
 }
--- Tic Tac Toe/WinGoal.cs
+++ Tic Tac Toe/WinGoal.cs
@@ -2,18 +2,30 @@
 {
     public class WinGoal : Goal
     {
+        // This class performs a check every time the player makes a move on the board.
+        // It will determine if the player has won the match.
+
+        // The moves of the player on the board.
         private bool[,] moves;
+
+        // The x coordinate of the player's move.
         private int x;
+
+        // The y coordinate of the player's move.
         private int y;
 
         public bool GoalReached()
         {
+            // Check if the player has won on the X/Y axis or in the left/right diagonal.
+
             return CheckHorizontal(moves, x) || CheckVertical(moves, y)
-                || CheckDiagLeft(moves) || CheckDiagRight(moves);
+                || CheckDiagRight(moves) || CheckDiagLeft(moves);
         }
 
         public void UpdateCurrentMove(bool[,] moves, int x, int y)
         {
+            // Updates the data of the current move made by the player.
+
             this.moves = moves;
             this.x = x;
             this.y = y;
@@ -21,8 +33,11 @@
 
         private bool CheckHorizontal(bool[,] moves, int x)
         {
+            // Check if the player has won on the X axis.
+
             for (int i = 0; i < MainForm.Y; i++)
             {
+                // If the player is missing one move then it's not a win.
                 if (!moves[x, i])
                     return false;
             }
@@ -32,20 +47,12 @@
 
         private bool CheckVertical(bool[,] moves, int y)
         {
-            for (int i = 0; i < MainForm.X; i++)
+            // Check if the player has won on the Y axis.
+
+            for (int i = 0; i < MainForm.Y; i++)
             {
+                // If the player is missing one move then it's not a win.
                 if (!moves[i, y])
-                    return false;
-            }
-
-            return true;
-        }
-
-        private bool CheckDiagLeft(bool[,] moves)
-        {
-            for (int i = 0; i < MainForm.X; i++)
-            {
-                if (!moves[i, i])
                     return false;
             }
 
@@ -54,8 +61,31 @@
 
         private bool CheckDiagRight(bool[,] moves)
         {
+            // Check if the player has won on the right diagonal.
+            //
+            // Basically we're checking these coordinates:
+            // (0, 0), (1, 1), (2, 2), (3, 3), (4, 4)
+
             for (int i = 0; i < MainForm.X; i++)
             {
+                // If the player is missing one move then it's not a win.
+                if (!moves[i, i])
+                    return false;
+            }
+
+            return true;
+        }
+
+        private bool CheckDiagLeft(bool[,] moves)
+        {
+            // Check if the player has won on the right diagonal.
+            //
+            // Basically we're checking these coordinates:
+            // (0, 4), (1, 3), (2, 2), (3, 1), (4, 0)
+
+            for (int i = 0; i < MainForm.X; i++)
+            {
+                // If the player is missing one move then it's not a win.
                 if (!moves[i, (MainForm.X - 1) - i])
                     return false;
             }
